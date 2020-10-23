e44ffd5d6e2cb8c8b0579f1736f2d9d104bdac23
Stephaniemascarenhas <stephanie.souza@aluno.faculdadeimpacta.com.br>
2020-10-22 23:07:55 -0300

    ac4

---
+++ app.py
@@ -0,0 +1,30 @@
+click==6.7
+Flask==1.0
+itsdangerous==0.24
+Jinja2==2.10.1
+Werkzeug==0.15.3
+Rafaela Oliveira de Sousa23:04
+import os
+from flask import Flask, jsonify, request
+from math import sqrt
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def fibonacci():
+
+    a = 1
+    b = 1
+    fib = '0,'
+
+    for q in range (50): 
+        fib += str(a) + ","
+        b = b + a
+        a = b - a
+    return fib
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
